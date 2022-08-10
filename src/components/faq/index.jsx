8f3af5f60f08fde477dd78be11e5cecff367288a
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2022-08-10 22:33:34 +0545

    accordion component half completed

--- src/components/faq/index.jsx
+++ src/components/faq/index.jsx
@@ -1,49 +1,84 @@
 import {
-  Accordion,
   AccordionDetails,
-  AccordionSummary,
   Typography,
 } from "@mui/material";
 import React from "react";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { AccordianTitle, AccordianWrapper, AccordionContent, AccordionContentSummary, AccordionTypography } from "../../styles/faq";
+import { BoxWrapper } from "../../styles/container";
 
 const Faq = () => {
   return (
     <>
-      <div>
-        <Accordion>
-          <AccordionSummary
+      <AccordianWrapper>
+        <BoxWrapper sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
+          <AccordianTitle variant="h3">Got Questions?</AccordianTitle>
+        <AccordionContent sx={{width: '100%'}}>
+          <AccordionContentSummary
             expandIcon={<ExpandMoreIcon />}
             aria-controls="panel1a-content"
             id="panel1a-header"
           >
-            <Typography>Accordion 1</Typography>
-          </AccordionSummary>
+            <AccordionTypography variant="h6">How do I create a playlist?</AccordionTypography>
+          </AccordionContentSummary>
           <AccordionDetails>
-            <Typography>
+            <Typography sx={{mt: '20px'}}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
               eget.
             </Typography>
           </AccordionDetails>
-        </Accordion>
-        <Accordion>
-          <AccordionSummary
+        </AccordionContent>
+        <AccordionContent >
+          <AccordionContentSummary
             expandIcon={<ExpandMoreIcon />}
             aria-controls="panel2a-content"
             id="panel2a-header"
           >
-            <Typography>Accordion 2</Typography>
-          </AccordionSummary>
+            <AccordionTypography variant="h6">How do I activate Data Saver mode?</AccordionTypography>
+          </AccordionContentSummary>
           <AccordionDetails>
-            <Typography>
+            <Typography sx={{mt: '20px'}}>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
               eget.
             </Typography>
           </AccordionDetails>
-        </Accordion>
-      </div>
+        </AccordionContent>  
+        <AccordionContent >
+          <AccordionContentSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <AccordionTypography variant="h6">Is it only possible to shuffle play music?</AccordionTypography>
+          </AccordionContentSummary>
+          <AccordionDetails>
+            <Typography sx={{mt: '20px'}}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </AccordionContent>  
+        <AccordionContent >
+          <AccordionContentSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <AccordionTypography variant="h6">Where can I find Podcasts?</AccordionTypography>
+          </AccordionContentSummary>
+          <AccordionDetails>
+            <Typography sx={{mt: '20px'}}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </AccordionContent>  
+        </BoxWrapper>
+      </AccordianWrapper>
     </>
   );
 };
---
+++ src/styles/faq/index.js
@@ -0,0 +1,35 @@
+import { Accordion, AccordionSummary, Box, Typography } from "@mui/material";
+import { styled } from "@mui/system";
+import { Colors } from "../theme";
+
+
+
+export const AccordianWrapper =styled(Box)(({theme}) => ({
+    padding: '50px 0px',
+}));
+
+export const AccordianTitle = styled(Typography)(() => ({
+    padding: '60px 0px',
+    color: Colors.black,
+    fontWeight: '500',
+}));
+
+export const AccordionContent = styled(Accordion)(() => ({
+    width: '100%',
+    padding: '15px',
+    marginBottom: 0,
+    
+}));
+
+export const AccordionContentSummary = styled(AccordionSummary)(() => ({
+    '&:hover':{
+        background: "#f5f5f5",
+    },
+}));
+
+export const AccordionTypography = styled(Typography)(() => ({
+    fontWeight: '500',
+    '&:hover':{
+        textDecoration: 'underline',
+    },
+}));
